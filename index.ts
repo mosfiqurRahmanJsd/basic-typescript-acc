cd72d61c65107de85e0596dfe80e20bb62343471
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-07-25 00:33:00 +0600

    object

--- index.ts
+++ index.ts
@@ -1,8 +1,30 @@
-// any, number, string, boolean, object, tuple, enum, undefined, null, void, never, unknown
+// any, number, string, boolean, object, tuple, enum, undefined, 
+// null, void, never, unknown
 let myName: string = 'Mosfiqur Rahman'; 
 let age: number; 
 age = 12; 
 let isHungry: boolean = false; 
 
+
+type StringOrNumber = number | string; 
+let studentId: StringOrNumber = 45245; 
+studentId = 'WEB4-45245'
+
+
+type PersonType = {name: string, age: number, hobby: string}
+
+
+let person: PersonType = {
+    name: 'Mosfiqur Rahman', 
+    age: 26, 
+    hobby: 'Sleeping'
+}
+
+
+let person2: PersonType = {
+    name: 'Someone', 
+    age: 26, 
+    hobby: 'codeing'
+}
 
 
