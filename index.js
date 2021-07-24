bebb5c273844db619e8b986c669d66c8cd42a4fe
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-07-25 00:19:20 +0600

    basic type

---
+++ index.js
@@ -0,0 +1,5 @@
+// any, number, string, boolean, object, tuple, enum, undefined, null, void, never, unknown
+var myName = 'Mosfiqur Rahman';
+var age;
+age = 12;
+var isHungry = false;
---
+++ index.ts
@@ -0,0 +1,8 @@
+// any, number, string, boolean, object, tuple, enum, undefined, null, void, never, unknown
+let myName: string = 'Mosfiqur Rahman'; 
+let age: number; 
+age = 12; 
+let isHungry: boolean = false; 
+
+
+
