960afb3bdc6a8892727f2c7b5b34b9a0577b69e7
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-07-27 21:59:12 +0600

    function added

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
-// any, number, string, boolean, object, tuple, enum, undefined, null, void, never, unknown
+"use strict";
+// any, number, string, boolean, object, tuple, enum, undefined, 
+// null, void, never, unknown
 var myName = 'Mosfiqur Rahman';
 var age;
 age = 12;
 var isHungry = false;
+var studentId = 45245;
+studentId = 'WEB4-45245';
+var person = {
+    name: 'Mosfiqur Rahman',
+    age: 26,
+    hobby: 'Sleeping'
+};
+var person2 = {
+    name: 'Someone',
+    age: 26,
+    hobby: 'COdeing'
+};
+// Array
+var numbers = [33, 40, 65, 85, 65, 'mosfiqur', 'shuvo'];
+var persons = [
+    {
+        name: 'mosfiqur',
+        age: 23,
+        hobby: 'codeing'
+    },
+    {
+        name: 'jhankar',
+        age: 23,
+        hobby: 'codeing'
+    },
+    {
+        name: 'solayman sadhin',
+        age: 23,
+        hobby: 'codeing'
+    }
+];
+// function
+var greeting = function (name) {
+    console.log("Hello " + name);
+};
+greeting('Mosfiqur');
+var add = function (a, b) {
+    return a + b;
+};
+var introduce = function (person) {
+    console.log("Hello, my name is " + person.name);
+};
+introduce({
+    name: 'mosfiqur rahman',
+    age: 26
+});
--- index.ts
+++ index.ts
@@ -11,20 +11,75 @@ let studentId: StringOrNumber = 45245;
 studentId = 'WEB4-45245'
 
 
+interface Person {
+    name: string, 
+    age: number,
+    hobby ?: string
+}
+
 type PersonType = {name: string, age: number, hobby: string}
 
 
-let person: PersonType = {
+let person: Person = {
     name: 'Mosfiqur Rahman', 
     age: 26, 
     hobby: 'Sleeping'
 }
 
 
-let person2: PersonType = {
+let person2: Person = {
     name: 'Someone', 
     age: 26, 
-    hobby: 'codeing'
+    hobby: 'COdeing'
 }
+
+
+// Array
+const numbers: (number | string)[] = [33, 40, 65, 85, 65, 'mosfiqur', 'shuvo']
+
+
+const persons: Person[] = [
+    {
+        name: 'mosfiqur', 
+        age: 23, 
+        hobby: 'codeing'
+    }, 
+    {
+        name: 'jhankar', 
+        age: 23, 
+        hobby: 'codeing'
+    }, 
+    {
+        name: 'solayman sadhin', 
+        age: 23, 
+        hobby: 'codeing'
+    }
+]
+
+
+
+
+// function
+
+const greeting = (name: string): void => {
+    console.log(`Hello ${name}`)
+}
+greeting('Mosfiqur');
+
+
+const add = (a: number, b: number): number => {
+    return a + b; 
+}
+
+
+const introduce = (person: Person): void => {
+    console.log(`Hello, my name is ${person.name}`)
+}
+
+introduce({
+    name: 'mosfiqur rahman', 
+    age: 26
+})
+
 
 
